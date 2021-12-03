6060a3034a31ca1feed9930711b2540e74a7f5ac
AnastasiiaShevchenkoK <93036160+AnastasiiaShevchenkoK@users.noreply.github.com>
2021-12-03 22:51:17 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,25 @@ import msg.First;
 public class Main {
 
     public static void main(String[] args) {
-//        Runtime rt = Runtime.instance();
-//        Profile p = new ProfileImpl();
-//        p.setParameter(Profile.MAIN_HOST, "localhost");
-//        p.setParameter(Profile.GUI, "true");
-//        ContainerController cc = rt.createMainContainer(p);
-//        //for(int i=0; i<6;i++){
-//        AgentController ac;
-//        try {
-//            AgentController ttt = cc.createNewAgent("First", "msg.First", null);
-//            ttt.start();
-//            ac = cc.createNewAgent("BookSellerAgent1", "bookTrading.BookSellerAgent", null);
-//            ac.start();
-//            ac = cc.createNewAgent("BookSellerAgent2", "bookTrading.BookSellerAgent", null);
-//            ac.start();
-//            ac = cc.createNewAgent("BookSellerAgent3", "bookTrading.BookSellerAgent", null);
-//            ac.start();
-//        } catch (StaleProxyException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//            //}
-//        }
+        Runtime rt = Runtime.instance();
+        Profile p = new ProfileImpl();
+        p.setParameter(Profile.MAIN_HOST, "localhost");
+        p.setParameter(Profile.GUI, "true");
+        ContainerController cc = rt.createMainContainer(p);
+        AgentController ac;
+        try {
+            AgentController ttt = cc.createNewAgent("First", "msg.First", null);
+            ttt.start();
+            ac = cc.createNewAgent("BookSellerAgent1", "bookTrading.BookSellerAgent", null);
+            ac.start();
+            ac = cc.createNewAgent("BookSellerAgent2", "bookTrading.BookSellerAgent", null);
+            ac.start();
+            ac = cc.createNewAgent("BookSellerAgent3", "bookTrading.BookSellerAgent", null);
+            ac.start();
+        } catch (StaleProxyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            //}
+        }
     }
 }
